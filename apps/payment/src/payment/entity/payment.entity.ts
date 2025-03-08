46ac5dc053e466f712d73d529069c97df0ca9e84
JongMany <blackberry1114@naver.com>
2025-03-08 23:51:37 +0900

    feat: payment msa

---
+++ apps/payment/src/payment/entity/payment.entity.ts
@@ -0,0 +1,56 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+export enum PaymentStatus {
+  pending = 'Pending',
+  rejected = 'Rejected',
+  approved = 'Approved',
+}
+
+export enum PaymentMethod {
+  creditCard = 'CreditCard',
+  kakao = 'Kakao',
+}
+
+export enum NotificationStatus {
+  pending = 'Pending',
+  send = 'Sent',
+}
+
+@Entity()
+export class Payment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    enum: PaymentStatus,
+    default: PaymentStatus.pending,
+  })
+  paymentStatus: PaymentStatus;
+
+  @Column({
+    enum: PaymentMethod,
+    default: PaymentMethod.creditCard,
+  })
+  paymentMethod: PaymentMethod;
+
+  @Column()
+  cardNumber: string;
+
+  @Column()
+  expiryYear: string;
+
+  @Column()
+  expirtyMonth: string;
+
+  @Column()
+  birthOrRegistration: string;
+
+  @Column()
+  passwordTwoDigits: string;
+
+  @Column({
+    enum: NotificationStatus,
+    default: NotificationStatus.pending,
+  })
+  notificationStatus: NotificationStatus;
+}
