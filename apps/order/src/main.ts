2a515d68fbbf8cf646054d6161e8af9059c1c1e3
JongMany <blackberry1114@naver.com>
2025-03-13 07:55:08 +0900

    refactor: close msa port

--- apps/order/src/main.ts
+++ apps/order/src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   });
   await app.startAllMicroservices();
 
-  await app.listen(process.env.HTTP_PORT ?? 3000);
+  // await app.listen(process.env.HTTP_PORT ?? 3000);
 }
 bootstrap()
   .then(() => {})
