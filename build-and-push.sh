37211ee7a492b7af8dbfbfd38275f3cc8fe878ca
JongMany <blackberry1114@naver.com>
2025-03-25 08:05:57 +0900

    feat: deploy workflow 추가

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker buildx build --platform linux/amd64,linux/arm64 -t jongmany/fc-nestjs-gateway -f ./apps/gateway/Dockerfile --target production .
-docker buildx build --platform linux/amd64,linux/arm64 -t jongmany/fc-nestjs-notification -f ./apps/notification/Dockerfile --target production .
-docker buildx build --platform linux/amd64,linux/arm64 -t jongmany/fc-nestjs-order -f ./apps/order/Dockerfile --target production .
-docker buildx build --platform linux/amd64,linux/arm64 -t jongmany/fc-nestjs-payment -f ./apps/payment/Dockerfile --target production .
-docker buildx build --platform linux/amd64,linux/arm64 -t jongmany/fc-nestjs-product -f ./apps/product/Dockerfile --target production .
-docker buildx build --platform linux/amd64,linux/arm64 -t jongmany/fc-nestjs-user -f ./apps/user/Dockerfile --target production .
+docker buildx build --platform linux/amd64,linux/arm64 -t jongmany/fc-nestjs-gateway:latest -f ./apps/gateway/Dockerfile --target production .
+docker buildx build --platform linux/amd64,linux/arm64 -t jongmany/fc-nestjs-notification:latest -f ./apps/notification/Dockerfile --target production .
+docker buildx build --platform linux/amd64,linux/arm64 -t jongmany/fc-nestjs-order:latest -f ./apps/order/Dockerfile --target production .
+docker buildx build --platform linux/amd64,linux/arm64 -t jongmany/fc-nestjs-payment:latest -f ./apps/payment/Dockerfile --target production .
+docker buildx build --platform linux/amd64,linux/arm64 -t jongmany/fc-nestjs-product:latest -f ./apps/product/Dockerfile --target production .
+docker buildx build --platform linux/amd64,linux/arm64 -t jongmany/fc-nestjs-user:latest -f ./apps/user/Dockerfile --target production .
 
 docker push jongmany/fc-nestjs-gateway:latest 
 docker push jongmany/fc-nestjs-notification:latest 
